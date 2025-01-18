888eefe4196380236be948dffccf174fd89b28d4
Gosia Komorowska <gosiakom2002@gmail.com>
2025-01-18 12:02:21 +0100

    clean up

--- audio_recording.py
+++ audio_recording.py
@@ -2,31 +2,17 @@ import librosa
 import pyaudio
 import numpy as np
 import wave
+
 def load_audio(file_path):
     y, sr = librosa.load(file_path)
     return y, sr
 
-
-def record_audio_pyaudio(filename, duration=10, fs=44100):
-    # import pyaudio
-    #
-    # p = pyaudio.PyAudio()
-    # for i in range(p.get_device_count()):
-    #     print(p.get_device_info_by_index(i))
-    # p.terminate()
-
+def record_audio_pyaudio(filename, duration=10, fs=48000): # fs=44100
     print("The recording has started...")
 
     p = pyaudio.PyAudio()
-    stream = p.open(format=pyaudio.paInt16,input_device_index=3, channels=1, rate=fs, input=True, frames_per_buffer=1024)
-    # stream = p.open(
-    #     format=pyaudio.paInt16,
-    #     channels=1,
-    #     rate=44100,
-    #     input=True,
-    #     input_device_index=1,  # Zamień na index Twojego mikrofonu
-    #     frames_per_buffer=1024
-    # )
+    stream = p.open(format=pyaudio.paInt16, channels=1, rate=fs, input=True, frames_per_buffer=1024, input_device_index=3)
+
     frames = []
     for i in range(0, int(fs / 1024 * duration)):
         data = stream.read(1024)
@@ -49,46 +35,4 @@ def record_audio_pyaudio(filename, duration=10, fs=44100):
     return recorded_data
 
 
-# def record_audio_pyaudio(filename, duration=10, fs=44100, device_index=None):
-#     print("The recording has started...")
-#
-#     p = pyaudio.PyAudio()
-#     stream = p.open(format=pyaudio.paInt16,
-#                     channels=1,
-#                     rate=fs,
-#                     input=True,
-#                     frames_per_buffer=1024,
-#                     input_device_index=device_index)
-#
-#     frames = []
-#     for i in range(0, int(fs / 1024 * duration)):
-#         data = stream.read(1024)
-#         frames.append(np.frombuffer(data, dtype=np.int16))
-#
-#     print("The recording has finished.")
-#
-#     stream.stop_stream()
-#     stream.close()
-#     p.terminate()
-#     recorded_data = np.concatenate(frames)
-#
-#     with wave.open(filename, 'wb') as wf:
-#         wf.setnchannels(1)
-#         wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
-#         wf.setframerate(fs)
-#         wf.writeframes(recorded_data.tobytes())
-#
-#     print(f"The recording has been saved in the file: {filename}")
-#     return recorded_data
-#
-#
-# def list_audio_devices():
-#     p = pyaudio.PyAudio()
-#     for i in range(p.get_device_count()):
-#         info = p.get_device_info_by_index(i)
-#         print(f"Device {i}: {info['name']}")
-#     p.terminate()
-
-
-
-# record_audio_pyaudio("user_sequences/elec3.wav", 4)
+record_audio_pyaudio("test.wav", 4)
